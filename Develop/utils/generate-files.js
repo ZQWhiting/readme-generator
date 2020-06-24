035c2cd8ec2ac27a43294c80513c0796028c015f
ZQWhiting <zach.whiting@icloud.com>
2020-06-23 20:16:28 -0600

    created generate-files module with promise functions

---
+++ Develop/utils/generate-files.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+// function to write README file
+function writeFile(markdown) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('../dist/README.md', markdown, err => {
+            if (err) {
+                reject(err);
+                return;
+            };
+
+            resolve({
+                ok: true,
+                message: 'ReadMe created!'
+            });
+        });
+    });
+}
+
+// function to copy LICENSE and Contributor Covenant READMEs.
+const copyLicense = (license, contributingSelect) => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile(`./src/${license}.md`, '../dist/LICENSE.md', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'License copied!'
+            });
+        })
+    });
+};
+
+const copyContributorCovenant = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile(`./src/contributor-covenant.md`, '../dist/contributor-covenant.md', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Contributor Covenant copied! '
+            });
+        })
+    });
+};
+
+module.exports = { writeFile, copyLicense, copyContributorCovenant }
